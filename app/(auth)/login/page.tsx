1218fdd734a40a40aed20d7291e454049f30f646
theBappy <abappy575@gmail.com>
2025-08-15 01:26:23 +0600

    github authentication and prisma with neon-postgres database setup

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader2Icon } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+export default function LoginPage() {
+  const [githubPending, startGithubTransition] = useTransition();
+
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected...");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
+        },
+      });
+    });
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>
+          Login with your Github or Email Account
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Button
+          disabled={githubPending}
+          onClick={signInWithGithub}
+          className="w-full"
+          variant="outline"
+        >
+          {githubPending ? (
+            <>
+              <Loader2Icon className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign with Github
+            </>
+          )}
+        </Button>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative text-muted-foreground z-10 bg-card px-2">
+            Or continue with
+          </span>
+        </div>
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="john@gmail.com" />
+          </div>
+          <Button>Continue with Email</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
