ea195e2603bcf655ef45ac23cf54ecae3c73d19a
theBappy <abappy575@gmail.com>
2025-08-16 10:45:46 +0600

    navigation and simple hero ui structure

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { Navbar } from "./_components/navbar";
+
+export default function LayoutPublic({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <Navbar />
+      <main className="container mx-auto px-4 md:px-6 lg:px-8">{children}</main>
+    </div>
+  );
+}
