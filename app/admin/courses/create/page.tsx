e6b7bdd5c403577a9a5e8267fc9ae099ed076ad4
theBappy <abappy575@gmail.com>
2025-08-18 10:58:39 +0600

    flle deletion from s3 bucket api

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -166,8 +166,7 @@ export default function CourseCreationPage() {
                   <FormItem className="w-full">
                     <FormLabel>Thumbnail Image</FormLabel>
                     <FormControl>
-                      <Uploader />
-                      {/* <Input placeholder="thumbnail url" {...field} /> */}
+                      <Uploader onChange={field.onChange} value={field.value}  />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
