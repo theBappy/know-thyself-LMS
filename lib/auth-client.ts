18a12078cb1067d9fd58ecef2c3e18cd37add1c0
theBappy <abappy575@gmail.com>
2025-08-18 12:27:01 +0600

    arcjet rules set-up for endpoint protection

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
+import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({plugins: [
-    emailOTPClient()
+    emailOTPClient(),
+    adminClient(),
 ]});
