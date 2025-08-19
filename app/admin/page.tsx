11a28316d6aed6544a6609630a07d274c5018f7b
theBappy <abappy575@gmail.com>
2025-08-19 12:15:38 +0600

    middleware implemented

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@ import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import data from "./data.json"
 
+
 export default async function AdminIndexPage() {
   
 
