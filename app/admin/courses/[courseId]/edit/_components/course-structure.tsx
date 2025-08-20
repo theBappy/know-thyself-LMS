361df7c38924ddf0a33f7f981b37af84c51bb007
theBappy <abappy575@gmail.com>
2025-08-20 08:54:43 +0600

    handleDragEnd proper indexing for both chapter and lesson to reorder with old and new index of the item

--- app/admin/courses/[courseId]/edit/_components/course-structure.tsx
+++ app/admin/courses/[courseId]/edit/_components/course-structure.tsx
@@ -43,6 +43,7 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { toast } from "sonner";
 
 interface Props {
   data: AdminCourseSingularType;
@@ -73,6 +74,7 @@ export function CourseStructure({ data }: Props) {
     })) || [];
 
   const [items, setItems] = useState(initialItems);
+  console.log(items);
 
   function SortableItem({ children, id, className, data }: SortableItemProps) {
     const {
@@ -104,15 +106,114 @@ export function CourseStructure({ data }: Props) {
   function handleDragEnd(event) {
     const { active, over } = event;
 
-    if (active.id !== over.id) {
-      setItems((items) => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
+    if (!over || active.id === over.id) {
+      return;
+    }
 
-        return arrayMove(items, oldIndex, newIndex);
-      });
+    const activeId = active.id;
+    const overId = over.id;
+    const activeType = active.data.current?.type as "chapter" | "lesson";
+    const overType = over.data.current?.type as "chapter" | "lesson";
+    const courseId = data.id;
+
+    if (activeType === "chapter") {
+      let targetChapterId = null;
+      if (overType === "chapter") {
+        targetChapterId = overId;
+      } else if (overType === "lesson") {
+        targetChapterId = over.data.current?.chapterId ?? null;
+      }
+      if (!targetChapterId) {
+        toast.error("Could not determine the chapter for reordering");
+        return;
+      }
+
+      const oldIndex = items.findIndex((item) => item.id === activeId);
+      const newIndex = items.findIndex((item) => item.id === targetChapterId);
+
+      if (oldIndex === -1 || newIndex === -1) {
+        toast.error("Could not find old/new index for reordering");
+        return;
+      }
+      const reorderedLocalChapters = arrayMove(items, oldIndex, newIndex);
+
+      const updatedChapterForState = reorderedLocalChapters.map(
+        (chapter, index) => ({
+          ...chapter,
+          order: index + 1,
+        })
+      );
+      const previousItems = [...items];
+      setItems(updatedChapterForState);
+    }
+
+    if (activeType === "lesson" && overType === "lesson") {
+      const chapterId = active.data.current?.chapterId;
+      const overChapterId = over.data.current?.chapterId;
+
+      if (!chapterId || chapterId !== overChapterId) {
+        toast.error(
+          "Lesson move between different chapters of invalid chapter ID is not allowed."
+        );
+        return;
+      }
+
+      const chapterIndex = items.findIndex(
+        (chapter) => chapter.id === chapterId
+      );
+
+      if (chapterIndex === -1) {
+        toast.error("Could not find chapter for lesson");
+        return;
+      }
+
+      const chapterToUpdate = items[chapterIndex];
+
+      const oldLessonIndex = chapterToUpdate.lessons.findIndex(
+        (lesson) => lesson.id === activeId
+      );
+
+      const newLessonIndex = chapterToUpdate.lessons.findIndex(
+        (lesson) => lesson.id === overId
+      );
+
+      if (oldLessonIndex === -1 || newLessonIndex === -1) {
+        toast.error("Could not find lesson for reordering");
+        return;
+      }
+
+      const reorderedLessons = arrayMove(
+        chapterToUpdate.lessons,
+        oldLessonIndex,
+        newLessonIndex
+      );
+
+      const updatedLessonForState = reorderedLessons.map((lesson, index) => ({
+        ...lesson,
+        order: index + 1,
+      }));
+
+      const newItems = [...items];
+
+      newItems[chapterIndex] = {
+        ...chapterToUpdate,
+        lessons: updatedLessonForState,
+      };
+
+      const previousItems = [...items]
+
+      setItems(newItems)
+
+
+      if(courseId){
+        const lessonsToUpdate = updatedLessonForState.map((lesson) => ({
+          id: lesson.id,
+          position: lesson.order,
+        }))
+      }
     }
   }
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -141,7 +242,7 @@ export function CourseStructure({ data }: Props) {
           <CardHeader className="flex flex-row items-center justify-between border-b border-border">
             <CardTitle>Chapters</CardTitle>
           </CardHeader>
-          <CardContent>
+          <CardContent className="space-y-8">
             <SortableContext
               strategy={verticalListSortingStrategy}
               items={items}
