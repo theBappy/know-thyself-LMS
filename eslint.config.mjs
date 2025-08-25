e01648873df7637f81119c814120d9af1a76ccfd
theBappy <abappy575@gmail.com>
2025-08-25 10:08:02 +0600

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
     ignores: ["./lib/generated/**"],
   },
 ];
