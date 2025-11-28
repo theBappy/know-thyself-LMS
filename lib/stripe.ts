b8e1064ab7fd7c7e5f117efbdbf0f2edf3c387d5
theBappy <abappy575@gmail.com>
2025-11-28 17:41:17 +0600

    api versions fixed of stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,6 +4,6 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-07-30.basil",
+  apiVersion: "2025-08-27.basil",
   typescript: true,
 });
