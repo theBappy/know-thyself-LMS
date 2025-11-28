35cf88dfb87114b5c05f0d14be814065f20d20b0
theBappy <abappy575@gmail.com>
2025-11-28 12:48:49 +0600

    removed separated type params in courseId

--- app/admin/courses/[courseId]/[chapterId]/[lessonId]/page.tsx
+++ app/admin/courses/[courseId]/[chapterId]/[lessonId]/page.tsx
@@ -1,17 +1,17 @@
 import { adminGetLesson } from "@/app/data/admin/admin-get-lesson";
 import { LessonForm } from "./_components/lesson-form";
 
-type Params = {
-  courseId: string;
-  chapterId: string;
-  lessonId: string;
-};
-
-export default async function LessonIdPage({ params }: { params: Params }) {
-  const { chapterId, lessonId, courseId } = params; 
+export default async function LessonIdPage({
+  params,
+}: {
+  params: {
+    courseId: string;
+    chapterId: string;
+    lessonId: string;
+  };
+}) {
+  const { courseId, chapterId, lessonId } = params;
   const lesson = await adminGetLesson(lessonId);
 
-  return (
-    <LessonForm data={lesson} chapterId={chapterId} courseId={courseId} />
-  );
+  return <LessonForm data={lesson} chapterId={chapterId} courseId={courseId} />;
 }
